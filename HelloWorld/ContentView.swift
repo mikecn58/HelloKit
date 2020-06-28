102839f98c42fec7267586c0eab4462793d9646d
mikehuang <mikehuang@126.com>
2020-06-28 14:46:58 +0800

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Mike Huang on 2020/6/28.
+//  Copyright © 2020 mike huang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
